cc3a9b2e0e15e889cca29cb43b295c8c51d24891
kirill <k.malkov@life-stream.tv>
2018-12-02 00:21:45 +0300

    add api for file unpload

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' })
 
 // require and use "multer"...
 
@@ -11,11 +13,16 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+  const file = req.file
+  res.json({name:file.originalname,type:file.mimetype,size:file.size});
 });
 
 app.listen(process.env.PORT || 3000, function () {
